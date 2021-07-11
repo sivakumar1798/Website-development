be8f6d47df018468d9aa662e070f4f6d50af3fa6
sivakumar1798 <60533368+sivakumar1798@users.noreply.github.com>
2021-07-11 10:02:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+      $('#menu').click(function(){
+         $(this).toggleClass('fa-time');
+		 $('.navbar').toggleClass('nav-toggle');
+	  });
+	  $('#login').click(function(){
+		  $('.login-form').addClass('popup');
+	  });
+	  $('.login-form form .fa-times').click(function(){
+		  $('.login-form').removeClass('popup');
+	  });
+	  $(window).on('load scroll',function(){
+		  
+		 $('#menu').removeClass('fa-time');
+		 $('.navbar').removeClass('nav-toggle');
+		 
+		  $('.login-form').removeClass('popup');
+		  
+		  $('section').each(function(){
+			  
+			  let top = $(window).scrollTop();
+			  let height = $(this).height();
+			  let id = $(this).attr('id');
+			  let offset= $(this).offset().top - 200;
+			  
+			  if(top > offset && top < offset + height){
+				  $('.navbar ul li a').removeClass('active');
+				  $('.navbar').find('[href="#${id}"]')addClass('active');
+			  }
+			  
+			  });
+			
+	  });
+
+});
